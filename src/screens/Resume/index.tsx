a8e5a717b6b72abaeeca228bcb1c79de626f127a
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-03-01 11:20:11 -0300

    create Resumo Screen and first history card

---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { HistoryCard } from "../../components/HistoryCard";
+import { Container, Header, Title } from "./styles";
+
+export function Resume() {
+  return (
+    <Container>
+      <Header>
+        <Title>Resumo por categoria</Title>
+      </Header>
+
+      <HistoryCard title="Comprar" amount="R$ 160, 23" color="red" />
+    </Container>
+  );
+}

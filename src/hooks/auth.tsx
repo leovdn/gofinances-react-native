726f1336714571885ea3da34c11a406c8a7bc4c1
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-03-02 12:32:59 -0300

    add environment variables

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,6 +1,8 @@
 import React, { createContext, ReactNode, useContext, useState } from "react";
-
 import * as AuthSession from "expo-auth-session";
+
+const { CLIENT_ID } = process.env;
+const { REDIRECT_URI } = process.env;
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -32,9 +34,6 @@ function AuthProvider({ children }: AuthProviderProps) {
 
   async function signInWithGoogle() {
     try {
-      const CLIENT_ID =
-        "474134333319-93r0jlac740gsjle9hsrn6ajtmj8h179.apps.googleusercontent.com";
-      const REDIRECT_URI = "https://auth.expo.io/@leovdn/gofinances";
       const RESPONSE_TYPE = "token";
       const SCOPE = encodeURI("profile email");
 
@@ -57,7 +56,7 @@ function AuthProvider({ children }: AuthProviderProps) {
           photo: userInfo.picture,
         });
       }
-    } catch (error) {
+    } catch (error: any) {
       throw new Error(error);
     }
   }
